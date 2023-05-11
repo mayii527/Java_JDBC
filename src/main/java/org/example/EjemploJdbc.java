5e252a62031819de1dc6cc8e48777c7625e994bc
Mayii <mayicadavid.527@gmail.com>
2023-05-11 12:16:37 -0500

    update

--- src/main/java/org/example/EjemploJdbc.java
+++ src/main/java/org/example/EjemploJdbc.java
@@ -1,21 +1,35 @@
 package org.example;
 
+import org.example.modelo.Producto;
 import org.example.Repositorio.ProductoRepositorioImpl;
 import org.example.Repositorio.Repositorio;
-import org.example.modelo.Producto;
 import org.example.util.ConexionBaseDatos;
 
 import java.sql.*;
+import java.util.Date;
 
 public class EjemploJdbc {
     public static void main(String[] args) {
-        try (/*Autoclose*/Connection conn = ConexionBaseDatos.getInstance()) {
+        try (Connection conn = ConexionBaseDatos.getInstance()) {
 
             Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
+            System.out.println("============= listar =============");
             repositorio.listar().forEach(System.out::println);
-            System.out.println(repositorio.porId(3L));
+
+            System.out.println("============= obtener por id =============");
+            System.out.println(repositorio.porId(1L));
+
+            System.out.println("============= insertar nuevo producto =============");
+            Producto producto = new Producto();
+            producto.setNombre("Teclado mecánico");
+            producto.setPrecio(500);
+            producto.setFechaRegistro(new Date());
+            repositorio.guardar(producto);
+            System.out.println("Producto guardado con éxito");
+            repositorio.listar().forEach(System.out::println);
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
